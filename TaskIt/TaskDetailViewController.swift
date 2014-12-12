be7c1a0c79039d6cf33364ff060a9285d738f57a
benobab <benlacroix83@gmail.com>
2014-12-12 18:55:56 +0100

    StoryBoardfonctionnelle1

--- TaskIt/TaskDetailViewController.swift
+++ TaskIt/TaskDetailViewController.swift
@@ -11,20 +11,18 @@ import UIKit
 class TaskDetailViewController: UIViewController {
 
     
-    @IBOutlet weak var dateLabel: UIDatePicker!
-    @IBOutlet weak var descriptionTaskLabel: UILabel!
-    @IBOutlet weak var titleTaskLabel: UILabel!
-    @IBOutlet weak var testLabel: UILabel!
     var detailTaskModel: TaskModel!
     
-    
+    @IBOutlet weak var dateLabel: UIDatePicker!
+    @IBOutlet weak var titleTask: UITextField!
+    @IBOutlet weak var descriptionTask: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.descriptionTaskLabel.text = detailTaskModel.subTask
-        self.titleTaskLabel.text = detailTaskModel.task
-        self.dateLabel = detailTaskModel
+        self.descriptionTask.text = detailTaskModel.subTask
+        self.titleTask.text = detailTaskModel.task
+        self.dateLabel.date = detailTaskModel.date
         
         // Do any additional setup after loading the view, typically from a nib.
         
